0358e9860230bec8d2eb235bc6748094afa1214b
mMRZZ <khanabadi.m.r@gmail.com>
2025-08-31 12:38:55 +0330

    chapter 14 completed

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,6 +2,7 @@ import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
 import { CreateInvoice } from "@/app/ui/invoices/buttons";
+import { notFound } from "next/navigation";
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
 	const params = await props.params;
@@ -12,6 +13,9 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
 		fetchCustomers(),
 	]);
 
+	if (!invoice) {
+		notFound;
+	}
 	return (
 		<main>
 			<Breadcrumbs
--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useActionState } from "react";
 import { CustomerField } from "@/app/lib/definitions";
 import Link from "next/link";
 import {
@@ -7,11 +9,13 @@ import {
 	UserCircleIcon,
 } from "@heroicons/react/24/outline";
 import { Button } from "@/app/ui/button";
-import { createInvoice } from "@/app/lib/actions";
+import { createInvoice, State } from "@/app/lib/actions";
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
+	const initialState: State = { message: null, errors: {} };
+	const [state, formaction] = useActionState(createInvoice, initialState);
 	return (
-		<form action={createInvoice}>
+		<form action={formaction}>
 			<div className='rounded-md bg-gray-50 p-4 md:p-6'>
 				{/* Customer Name */}
 				<div className='mb-4'>
@@ -25,7 +29,8 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
 							id='customer'
 							name='customerId'
 							className='peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500'
-							defaultValue=''>
+							defaultValue=''
+							aria-describedby='customer-error'>
 							<option value='' disabled>
 								Select a customer
 							</option>
@@ -36,6 +41,19 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
 							))}
 						</select>
 						<UserCircleIcon className='pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500' />
+					</div>
+					<div
+						id='customer-error'
+						aria-live='polite'
+						aria-atomic='true'>
+						{state.errors?.customerId &&
+							state.errors.customerId.map((error: string) => (
+								<p
+									className='mt-2 text-sm text-red-500'
+									key={error}>
+									{error}
+								</p>
+							))}
 					</div>
 				</div>
 
@@ -55,9 +73,23 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
 								step='0.01'
 								placeholder='Enter USD amount'
 								className='peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500'
+								aria-describedby='amount-error'
 							/>
 							<CurrencyDollarIcon className='pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900' />
 						</div>
+					</div>
+					<div
+						id='amount-error'
+						aria-live='polite'
+						aria-atomic='true'>
+						{state.errors?.amount &&
+							state.errors.amount.map((error: string) => (
+								<p
+									className='mt-2 text-sm text-red-500'
+									key={error}>
+									{error}
+								</p>
+							))}
 					</div>
 				</div>
 
@@ -74,6 +106,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
 									name='status'
 									type='radio'
 									value='pending'
+									aria-describedby='status-error'
 									className='h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2'
 								/>
 								<label
@@ -88,6 +121,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
 									name='status'
 									type='radio'
 									value='paid'
+									aria-describedby='status-error'
 									className='h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2'
 								/>
 								<label
@@ -99,6 +133,25 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
 						</div>
 					</div>
 				</fieldset>
+				<div id='status-error' aria-live='polite' aria-atomic='true'>
+					{state.errors?.status &&
+						state.errors.status.map((error: string) => (
+							<p
+								className='mt-2 text-sm text-red-500'
+								key={error}>
+								{error}
+							</p>
+						))}
+				</div>
+			</div>
+			<div id='missing-data-error' aria-live='polite' aria-atomic='true'>
+				{state.errors && (
+					<p
+						className='mt-2 text-sm text-red-500'
+						key={state.message}>
+						{state.message}
+					</p>
+				)}
 			</div>
 			<div className='mt-6 flex justify-end gap-4'>
 				<Link
